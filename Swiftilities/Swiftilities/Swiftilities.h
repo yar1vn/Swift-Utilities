8ced2a567999cd7eb8c6304294b7ff9ca788444d
Yariv Nissim <yariv.nis@gmail.com>
2015-05-10 16:51:38 -0700

    Initial Commit

---
+++ Swiftilities/Swiftilities/Swiftilities.h
@@ -0,0 +1,19 @@
+//
+//  Swiftilities.h
+//  Swiftilities
+//
+//  Created by Yariv on 5/10/15.
+//  Copyright (c) 2015 Yariv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swiftilities.
+FOUNDATION_EXPORT double SwiftilitiesVersionNumber;
+
+//! Project version string for Swiftilities.
+FOUNDATION_EXPORT const unsigned char SwiftilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiftilities/PublicHeader.h>
+
+
